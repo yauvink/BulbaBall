8d2a5b40b4ab81aef9784054641aa604239c6400
Yauhen Vinkevich <yau.vink@gmail.com>
2019-04-08 20:07:07 +0300

    add project to git

---
+++ js/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+   function autorun()
+   {
+        // alert("loh")
+   }
+   if (window.addEventListener) window.addEventListener("load", autorun, false);
+   else if (window.attachEvent) window.attachEvent("onload", autorun);
+   else window.onload = autorun;
+
+
+
+
+
+
+
+
