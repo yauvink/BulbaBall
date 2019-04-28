6c1d5710c3d42e76d2b407db175402b809ca6241
boozzy <yau.vink@gmail.com>
2019-04-28 22:22:49 +0300

    add jquery

--- js/main.js
+++ js/main.js
@@ -1,57 +1,56 @@
 'use strict';
 
-  //  function autorun()
-  //  {
-  //    var ytData;
-  //     $.getJSON("https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&channelId=UCnRisinlQ3FiupZH8B_TJiw&maxResults=6&key=AIzaSyD40HdMjRci08Vh41DBfniJAsQomaY7PLQ", getData);
-  //     function getData(data){
-  //         ytData = data;
-  //         // console.log(ytData);
-  //         console.log(ytData.items[0].snippet.title)
-  //         console.log("id = " + ytData.items[0].id.videoId)
-  //         // console.log("hello world")
-  //         // document.getElementById("test-v-id").innerHTML = ytData.items[0].snippet.title;
-  //     }
-  //  }
-  //  if (window.addEventListener) window.addEventListener("load", autorun, false);
-  //  else if (window.attachEvent) window.attachEvent("onload", autorun);
-  //  else window.onload = autorun;
-
-
+//  function autorun()
+//  {
+//    var ytData;
+//     $.getJSON("https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&channelId=UCnRisinlQ3FiupZH8B_TJiw&maxResults=6&key=AIzaSyD40HdMjRci08Vh41DBfniJAsQomaY7PLQ", getData);
+//     function getData(data){
+//         ytData = data;
+//         // console.log(ytData);
+//         console.log(ytData.items[0].snippet.title)
+//         console.log("id = " + ytData.items[0].id.videoId)
+//         // console.log("hello world")
+//         // document.getElementById("test-v-id").innerHTML = ytData.items[0].snippet.title;
+//     }
+//  }
+//  if (window.addEventListener) window.addEventListener("load", autorun, false);
+//  else if (window.attachEvent) window.attachEvent("onload", autorun);
+//  else window.onload = autorun;
 
 const url = "https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&channelId=UCnRisinlQ3FiupZH8B_TJiw&maxResults=6&key=AIzaSyD40HdMjRci08Vh41DBfniJAsQomaY7PLQ";
-const previewUrl = 'https://img.youtube.com/vi/${videoId}/0.jpg';
-var reviewsWrap = document.getElementById("reviewsWrap");
 
-fetch(url).then(function(response) {
+
+fetch(url).then(function (response) {
   return response.json();
-}).then(function(data) {
-  console.log(data)
-  // const items = data.items;
-  for(var i = 0; i < data.items.length; i++){
+}).then(function (data) {
+  for (let i = 0; i < data.items.length; i++) {
 
-    var link = document.createElement("a");
-    link.href = "https://www.youtube.com/watch?v=" + data.items[i].id.videoId;
-    link.className = "review";
-    link.target = "_blank";
+    // создаем ссылку-обертку
+    let link = $("<a>").toggleClass("review").attr({
+      href: "https://www.youtube.com/watch?v=" + data.items[i].id.videoId,
+      target: "_blank"
+    });
 
-    var divReview = document.createElement("div");
-    divReview.className = "review"; 
+    // создаем див-обертку
+    let review = $("<div>").toggleClass("review");
+
+    // создаем див с превью
+    let reviewPreview = $("<div>").toggleClass("review-preview").css({
+      backgroundImage: "url('//img.youtube.com/vi/" + data.items[i].id.videoId + "/mqdefault.jpg')"
+    });
     
-    var divReviewPreview = document.createElement("div");
-    divReviewPreview.className = "review-preview";
-    divReviewPreview.style.backgroundImage = "url('//img.youtube.com/vi/"+ data.items[i].id.videoId +"/mqdefault.jpg')";
+    // создаем тайтл
+    let reviewTitle = $("<h5>").html(data.items[i].snippet.title);
 
-    var title = document.createElement("h5");
-    title.innerHTML = data.items[i].snippet.title;
+    // добавляем все в блок
+    $(review).append(reviewPreview).append(reviewTitle);
 
-    divReview.appendChild(divReviewPreview);
-    divReview.appendChild(title);
-    link.appendChild(divReview);
-    reviewsWrap.appendChild(link);
-    
+    $(link).append(review);
+
+    $(".reviews-inner").append(link);
+
   }
-  
+
   // const previews = items.map(item => {
   //   return {
   //     description: '',
@@ -60,5 +59,7 @@ fetch(url).then(function(response) {
   // });
   // console.log(previews);
 });
+
+// $(document).append($("div").toggleClass("testdiv"))
 
 
