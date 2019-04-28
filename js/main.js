2b01b224e5a82c52a28c07a38c3aca31147ecf81
boozzy <yau.vink@gmail.com>
2019-04-28 23:50:42 +0300

    0.0.1 jq

--- js/main.js
+++ js/main.js
@@ -1,21 +1,5 @@
 'use strict';
 
-//  function autorun()
-//  {
-//    var ytData;
-//     $.getJSON("https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&channelId=UCnRisinlQ3FiupZH8B_TJiw&maxResults=6&key=AIzaSyD40HdMjRci08Vh41DBfniJAsQomaY7PLQ", getData);
-//     function getData(data){
-//         ytData = data;
-//         // console.log(ytData);
-//         console.log(ytData.items[0].snippet.title)
-//         console.log("id = " + ytData.items[0].id.videoId)
-//         // console.log("hello world")
-//         // document.getElementById("test-v-id").innerHTML = ytData.items[0].snippet.title;
-//     }
-//  }
-//  if (window.addEventListener) window.addEventListener("load", autorun, false);
-//  else if (window.attachEvent) window.attachEvent("onload", autorun);
-//  else window.onload = autorun;
 
 const url = "https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&channelId=UCnRisinlQ3FiupZH8B_TJiw&maxResults=6&key=AIzaSyD40HdMjRci08Vh41DBfniJAsQomaY7PLQ";
 
@@ -32,34 +16,25 @@ fetch(url).then(function (response) {
     });
 
     // создаем див-обертку
-    let review = $("<div>").toggleClass("review");
+    let review = $("<div>").toggleClass("reviewWrap");
 
-    // создаем див с превью
-    let reviewPreview = $("<div>").toggleClass("review-preview").css({
-      backgroundImage: "url('//img.youtube.com/vi/" + data.items[i].id.videoId + "/mqdefault.jpg')"
+    // создаем превью
+    // let reviewPreview = $("<img>").toggleClass("review-preview").css({
+    //   backgroundImage: "url('//img.youtube.com/vi/" + data.items[i].id.videoId + "/mqdefault.jpg')"
+    // });
+    let reviewPreview = $("<img>").toggleClass("review-preview").attr({
+      src: "//img.youtube.com/vi/" + data.items[i].id.videoId + "/mqdefault.jpg"
     });
     
     // создаем тайтл
     let reviewTitle = $("<h5>").html(data.items[i].snippet.title);
 
-    // добавляем все в блок
+    // собираем в ссылку
     $(review).append(reviewPreview).append(reviewTitle);
-
     $(link).append(review);
-
+    // добавляем на страницу
     $(".reviews-inner").append(link);
 
   }
-
-  // const previews = items.map(item => {
-  //   return {
-  //     description: '',
-  //     imageSrc: previewUrl.replace('${videoId}', item.id.videoId),
-  //   };
-  // });
-  // console.log(previews);
 });
-
-// $(document).append($("div").toggleClass("testdiv"))
-
 
