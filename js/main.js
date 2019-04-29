e8e461948dc75d7fb098832b1fd8bf3587656222
boozzy <yau.vink@gmail.com>
2019-04-29 23:47:50 +0300

    0.0.3 mdl on

--- js/main.js
+++ js/main.js
@@ -4,34 +4,69 @@
 const url = "https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&channelId=UCnRisinlQ3FiupZH8B_TJiw&maxResults=6&key=AIzaSyD40HdMjRci08Vh41DBfniJAsQomaY7PLQ";
 
 
+// fetch(url).then(function (response) {
+//   return response.json();
+// }).then(function (data) {
+//   for (let i = 0; i < data.items.length; i++) {
+
+//     // создаем ссылку-обертку
+//     let link = $("<a>").addClass("review").attr({
+//       href: `https://www.youtube.com/watch?v=${data.items[i].id.videoId}`,
+//       target: "_blank"
+//     });
+
+//     // создаем див-обертку
+//     let review = $("<div>").addClass("reviewWrap");
+
+//     // создаем превью
+//     let reviewPreview = $("<img>").addClass("review-preview").attr({
+//       src: `//img.youtube.com/vi/${data.items[i].id.videoId}/mqdefault.jpg`
+//     });
+    
+//     // создаем тайтл
+//     let reviewTitle = $("<h5>").html(data.items[i].snippet.title);
+
+//     // собираем в ссылку
+//     review.append(reviewPreview).append(reviewTitle);
+//     link.append(review);
+//     // добавляем на страницу
+//     $(".reviews-inner").append(link);
+
+//   }
+// });
+
 fetch(url).then(function (response) {
   return response.json();
 }).then(function (data) {
   for (let i = 0; i < data.items.length; i++) {
 
-    // создаем ссылку-обертку
-    let link = $("<a>").addClass("review").attr({
-      href: `https://www.youtube.com/watch?v=${data.items[i].id.videoId}`,
-      target: "_blank"
-    });
+  //  создаем div обертку
+  let mdlCell = $('<div>').addClass('review-wrap mdl-cell mdl-cell--4-col mdl-card mdl-shadow--2dp');
 
-    // создаем див-обертку
-    let review = $("<div>").addClass("reviewWrap");
 
-    // создаем превью
-    let reviewPreview = $("<img>").addClass("review-preview").attr({
-      src: `//img.youtube.com/vi/${data.items[i].id.videoId}/mqdefault.jpg`
-    });
-    
-    // создаем тайтл
-    let reviewTitle = $("<h5>").html(data.items[i].snippet.title);
+  // cоздаем ссылку??? обертку
+  let review = $('<a>').addClass('review').attr({
+          href: `https://www.youtube.com/watch?v=${data.items[i].id.videoId}`,
+          target: "_blank"
+        });
 
-    // собираем в ссылку
-    review.append(reviewPreview).append(reviewTitle);
-    link.append(review);
-    // добавляем на страницу
-    $(".reviews-inner").append(link);
+  // создаем превью
+  let reviewPreview = $("<img>").addClass("review-preview").attr({
+    src: `//img.youtube.com/vi/${data.items[i].id.videoId}/mqdefault.jpg`
+  });
+  
+ // создаем тайтл
+ let reviewTitle = $("<h6>").addClass('review-title').html(data.items[i].snippet.title);
+
+
+//  собираем вместе 
+
+review.append(reviewPreview).append(reviewTitle);
+mdlCell.append(review);
+$('.reviews-inner').append(mdlCell);
+
 
   }
 });
+
 
